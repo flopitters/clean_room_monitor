0e821317ed50c6bd381d1d55feb8863c9acbf72d
fpipper <florian.pipper@cern.ch>
2018-09-12 11:14:14 +0200

    Added flags for use of devices.

--- logger/logger.py
+++ logger/logger.py
@@ -34,6 +34,10 @@ class clean_room_monitor(object):
         self.mano_meter_pin = 7                 # gpio pin of pressure monitor
         self.counter_address = '/dev/ttyUSB0'   # serial port particle counter
 
+        self.use_dht22 = 0                      # flag if dht is connected
+        self.use_bmp180 = 0                     # flag if bmp is connected
+        self.use_dc1700 = 0                     # flag if dylos is connected
+
         self.wait = 10                          # time between measurements
                                                 # keep in mind that the pc measurement takes 60s
 
@@ -46,8 +50,10 @@ class clean_room_monitor(object):
         try:
             while fContinous:
                 ## Set up devices
-                dc1700 = dc.dc1700(self.counter_address)
-                #bmp180 = bmp085.BMP085()
+                if self.use_dc1700:
+                    dc1700 = dc.dc1700(self.counter_address)
+                if self.use_bmp180:
+                    bmp180 = bmp085.BMP085()
 
                 ## Prepare
                 hd = '# Date | Time | Temperature [C] | Humidity [%]| | Pressure [Bar] | Particles > 0.5 um [m^-3] | Particles > 2.5 um [m^-3] | ISO | Class\n'
@@ -55,11 +61,16 @@ class clean_room_monitor(object):
                 ## Read measurements
                 date = time.strftime("%Y/%m/%d", time.localtime())
                 clock = time.strftime("%H:%M:%S", time.localtime())
-                hum, temp = dht22.read_retry(22, self.hum_meter_pin)
-                #pres = bmp180.read_pressure()
-                cnt05, cnt25 = dc1700.read_particle_counts() # takes 60s of integration time
-                pres = -1
-                
+
+                hum = temp = pres = cnt05 = cnt25 = -1
+
+                if self.use_dht22:
+		            hum, temp = dht22.read_retry(22, self.hum_meter_pin)
+                if self.use_bmp180:
+                    pres = bmp180.read_pressure()
+                if self.use_dc1700:
+                    cnt05, cnt25 = dc1700.read_particle_counts() # takes 60s of integration time
+
                 ## Determine cleanroom class from particle count
                 if (cnt05 < 352000 and cnt25 < 11720):
                     iso = 7
@@ -83,7 +94,7 @@ class clean_room_monitor(object):
                 ## Create log folder if it dpes not already exist
                 if not (os.path.isdir('logs/')):
                     os.makedirs('logs')
-                
+
                 ## Create file if it does not already exists
                 if not (os.path.isfile('logs/' + file_name + '.txt')):
                     file = open('logs/' + file_name + '.txt', 'w')
@@ -94,7 +105,7 @@ class clean_room_monitor(object):
                 file = open('logs/' + file_name + '.txt', 'a')
                 file.write('{:s}  {:s}  {:3.1f}%  {:3.1f}C  {:d}  {:.0f}  {:.0f}  {:d}  {:d}\n'.format(*line))
                 file.close()
-                
+
                 if fSingle:
                     fContinous = 0
                 else:
