f9f93aa1858108cb44d2dcfa870389d8d127c868
twentycrooks <florian.pitters@gmail.com>
2018-10-31 15:16:04 +0100

    Final fix data_path

--- logger/logger.py
+++ logger/logger.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python
 
 # ============================================================================
-# File: monitor.py
+# File: logger.py
 # ------------------------------
 #
 # Clean room monitor based on rpi with dht22, bmp186 and dc17000.
 #
 # Notes:
-#   -
-#
-# Layout:
-#   configure and prepare
+#   - Dylos DC1700 takes 60 second to integrate
 #
 # Status:
-#   in progress
+#   works well
 #
 # Author: Florian Pitters
 #
 # ============================================================================
 
 import time, os, sys
+from optparse import OptionParser
+
 import dc1700 as dc
 import Adafruit_DHT as dht22
 import Adafruit_BMP.BMP085 as bmp085
 
 
-class clean_room_monitor:
+class clean_room_monitor(object):
     """ Log humidty, temperature, pressure and particle count. """
 
-    def __init__(self,dat_path):
-        self.dat_path = dat_path        
-        
-    def initialise(self):
+    def __init__(self, dat_path):
+
+        self.dat_path = dat_path                # data path
+
         self.hum_meter_pin = 4                  # gpio pin of humidity monitor
         self.mano_meter_pin = 7                 # gpio pin of pressure monitor
         self.counter_address = '/dev/ttyUSB0'   # serial port particle counter
@@ -67,13 +66,13 @@ class clean_room_monitor:
                 hum = temp = pres = cnt05 = cnt25 = -1
 
                 if self.use_dht22:
-		    hum, temp = dht22.read_retry(22, self.hum_meter_pin)
+		            hum, temp = dht22.read_retry(22, self.hum_meter_pin)
                 if self.use_bmp180:
                     pres = bmp180.read_pressure()
                 if self.use_dc1700:
                     cnt05, cnt25 = dc1700.read_particle_counts() # takes 60s of integration time
 
-                ## Determine cleanroom class from particle count
+                ## Determine cleanroom class from particle count per cubic inch
                 if (cnt05/0.0254**3 < 352000 and cnt25/0.0254**3 < 11720):
                     iso = 7
                     clas = 10000
@@ -88,24 +87,24 @@ class clean_room_monitor:
 
                 ## Print feedback
                 if fPrint:
-                    print '{:s}  {:s}  {:3.1f}%  {:3.1f}C  {:d}  {:.0f}  {:.0f}  {:d}  {:d}'.format(*line)
+                    print '{:s}  {:s}  {:3.1f}  {:3.1f}  {:d}  {:.0f}  {:.0f}  {:d}  {:d}'.format(*line)
 
                 ## One file per day
                 file_name = time.strftime("%Y_%m_%d", time.localtime())
 
                 ## Create log folder if it dpes not already exist
-                if not (os.path.isdir('logs/')):
-                    os.makedirs('logs')
+                if not (os.path.isdir(self.dat_path)):
+                    os.makedirs(self.dat_path)
 
                 ## Create file if it does not already exists
-                if not (os.path.isfile(self.dat_path + file_name + '.txt')):
-                    file = open(self.dat_path  + file_name + '.txt', 'w')
+                if not (os.path.isfile(self.dat_path + '/' + file_name + '.txt')):
+                    file = open(self.dat_path + '/' + file_name + '.txt', 'w')
                     file.write(hd)
                     file.close()
 
                 ## Append file
-                file = open(self.dat_path + file_name + '.txt', 'a')
-                file.write('{:s}  {:s}  {:3.1f}%  {:3.1f}C  {:d}  {:.0f}  {:.0f}  {:d}  {:d}\n'.format(*line))
+                file = open(self.dat_path + '/' + file_name + '.txt', 'a')
+                file.write('{:s}  {:s}  {:3.1f}  {:3.1f}  {:d}  {:.0f}  {:.0f}  {:d}  {:d}\n'.format(*line))
                 file.close()
 
                 if fSingle:
@@ -120,9 +119,22 @@ class clean_room_monitor:
 
 
 
-if __name__=="__main__":
 
-    dat_path = '/home/hgsensor/Applications/clean_room_monitor/data/'
-    run = clean_room_monitor(dat_path)
-    run.initialise()
+# Main app
+# -------------------------------------
+
+def main():
+    usage = "usage: ./logger.py -d [data_path]"
+
+    parser = OptionParser(usage=usage, version="prog 0.01")
+    parser.add_option("-d", "--dat_path", action="store", dest="dat_path", type="string", default="logs/",  help="data path")
+
+    (options, args) = parser.parse_args()
+    if not (options.dat_path):
+	       parser.error("You need to give a data path. Try '-h' for more info.")
+
+    run = clean_room_monitor(options.dat_path)
     run.execute()
+
+if __name__=="__main__":
+	main()
